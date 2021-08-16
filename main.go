6d284a60550c77e345f1351f9241ac0e0a6ebabc
andy <andy82573495@gmail.com>
2021-08-16 12:21:43 +0800

    X可以在第一格執行

--- main.go
+++ main.go
@@ -5,31 +5,43 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"strconv"
 	"strings"
 )
 
+const initApp = 0
+
+var board = [15]string{" ", "|", " ", "|", " \n", " ", "|", " ", "|", " \n", " ", "|", " ", "|", " \n"}
+
 func main() {
-	drawScaffold()
-	fmt.Print("Choose a position from 1 to 9:")
+	drawScaffold(initApp)
 	reader := bufio.NewReader(os.Stdin)
-	number, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+	for {
+		fmt.Print("Choose a position from 1 to 9:")
+		number, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		number = strings.TrimSpace(number)
+		posi, err := strconv.Atoi(number)
+		drawScaffold(posi)
 	}
-	number = strings.TrimSpace(number)
-	posi, err := strconv.Atoi(number)
-	fmt.Print(posi)
-	clearScreen()
+	// clearScreen()
 }
-func drawScaffold() {
-	fmt.Println(" | | ")
-	fmt.Println(" | | ")
-	fmt.Println(" | | ")
+func drawScaffold(posi int) {
+	switch posi {
+	case 1:
+		board[0] = "X"
+
+	}
+	for _, v := range board {
+		fmt.Print(v)
+	}
+
 }
-func clearScreen() {
-	cmd := exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-}
+
+// func clearScreen() {
+// 	cmd := exec.Command("clear")
+// 	cmd.Stdout = os.Stdout
+// 	cmd.Run()
+// }
