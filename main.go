262bc5cdbe26e17f34626ac5548383fbf422563e
andy <andy82573495@gmail.com>
2021-08-23 17:31:17 +0800

    finished

--- main.go
+++ main.go
@@ -1,33 +1,97 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
 )
 
-type game struct {
+type Game struct {
 	board  [9]string
 	player string
+	turn   int
 }
 
 func main() {
-	var newGame game
-	printBoard(newGame.board)
-	posi, err := inputPosition()
-	if err != nil {
-		log.Fatal(err)
+	var game Game
+	gameOver := false
+	game.player = "X"
+	var winner string
+	for !gameOver {
+		printBoard(game.board)
+		posi := inputPosition()
+		err := game.playGame(posi)
+		if err != nil {
+			fmt.Println(err)
+			continue //skip next step
+		}
+		gameOver, winner = game.checkGameWinner()
 	}
-	fmt.Println(posi)
-
+	printBoard(game.board)
+	if winner == "" {
+		fmt.Println("It`s a tie.")
+	} else {
+		fmt.Printf("Winner is %s.\n", winner)
+	}
 }
-func inputPosition() (int, error) {
+
+//switch player
+func (game *Game) switchPlayer() {
+	if game.player == "X" {
+		game.player = "O"
+	} else {
+		game.player = "X"
+	}
+}
+
+//insert posi
+func (game *Game) playGame(posi int) error {
+	if game.board[posi-1] != "" {
+		return errors.New("Try another move.")
+	}
+	game.board[posi-1] = game.player
+	game.turn++
+	game.switchPlayer()
+	return nil
+}
+
+//return whether game is over and who is winner
+func (game Game) checkGameWinner() (bool, string) {
+	board := game.board
+	//horizontal
+	for i := 0; i < 9; {
+		if board[i] != "" && board[i] == board[i+1] && board[i] == board[i+2] {
+			return true, board[i]
+		} else {
+			i += 3
+		}
+	}
+	//vertical
+	for i := 0; i < 3; i++ {
+		if board[i] != "" && board[i] == board[i+3] && board[i] == board[i+6] {
+			return true, board[i]
+		}
+	}
+	//diagonal 0,4,8 and 2,4,6
+	if board[0] != "" && board[0] == board[4] && board[0] == board[8] {
+		return true, board[0]
+	}
+	if board[2] != "" && board[2] == board[4] && board[2] == board[6] {
+		return true, board[0]
+	}
+	if game.turn == 9 {
+		return true, ""
+	}
+
+	return false, ""
+}
+
+//return player input number
+func inputPosition() int {
 	var posi int
 	fmt.Print("Enter a position from 1 to 9:")
-	_, err := fmt.Scan(&posi)
-	if err != nil {
-		return posi, nil
-	}
-	return posi, nil
+	fmt.Scan(&posi)
+
+	return posi
 }
 func printBoard(board [9]string) {
 	for i, v := range board {
